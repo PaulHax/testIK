adec9981bf49ca8a54be1815dac9bb4516742088
Paul <gladsomebeast@gmail.com>
2020-01-27 21:03:14 -0800

    Updates

--- build.js
+++ build.js
@@ -1,8 +1,7 @@
-(function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? factory(require('three')) :
-	typeof define === 'function' && define.amd ? define(['three'], factory) :
-	(global = global || self, factory(global.THREE));
-}(this, (function (three) { 'use strict';
+(function (factory) {
+	typeof define === 'function' && define.amd ? define(factory) :
+	factory();
+}((function () { 'use strict';
 
 	if ( Number.EPSILON === undefined ) {
 		Number.EPSILON = Math.pow( 2, - 52 );
@@ -30355,13 +30354,8 @@ void main() {
 		return RoughnessMipmapper;
 	} )();
 
-	(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.head.appendChild(r); })(window.document);
-	const t = new three.Vector3();
-	const q = new three.Quaternion();
-	const FORWARD = new three.Vector3(0,0,1);
-	var RESETQUAT = new three.Quaternion();
-	var ROT = new three.Quaternion().setFromAxisAngle(new three.Vector3(1, 0, 0), Math.PI/2);
-	var ROTI = new three.Quaternion().setFromAxisAngle(new three.Vector3(1, 0, 0), -Math.PI/2);
+	var ROT = new Quaternion().setFromAxisAngle(new Vector3(1, 0, 0), Math.PI/2);
+	var ROTI = new Quaternion().setFromAxisAngle(new Vector3(1, 0, 0), -Math.PI/2);
 	function setZForward(parsedActor) {
 	  var worldPos = {};
 	  getOriginalWorldPositions(parsedActor.hips, worldPos);
@@ -30369,10 +30363,10 @@ void main() {
 	  parsedActor.hips.updateMatrixWorld();
 	  parsedActor.shoulderR.quaternion.multiply(ROTI);
 	  parsedActor.shoulderR.quaternion
-	    .premultiply(new three.Quaternion().setFromAxisAngle(new three.Vector3(1, 0, 0), Math.PI/2));
+	    .premultiply(new Quaternion().setFromAxisAngle(new Vector3(1, 0, 0), Math.PI/2));
 	  parsedActor.shoulderL.quaternion.multiply(ROTI);
 	  parsedActor.shoulderL.quaternion
-	    .premultiply(new three.Quaternion().setFromAxisAngle(new three.Vector3(1, 0, 0), Math.PI/2));
+	    .premultiply(new Quaternion().setFromAxisAngle(new Vector3(1, 0, 0), Math.PI/2));
 	  updatePos(parsedActor.hips, worldPos);
 	}
 	function updatePos(parentBone, worldPos) {
@@ -30388,32 +30382,59 @@ void main() {
 	}
 	function getOriginalWorldPositions(rootBone, worldPos) {
 	  rootBone.children.forEach((child) => {
-	    var childWorldPos = child.getWorldPosition(new three.Vector3());
+	    var childWorldPos = child.getWorldPosition(new Vector3());
 	    worldPos[child.id] = childWorldPos;
 	    getOriginalWorldPositions(child, worldPos);
 	  });
 	}
-	const localVector = new three.Vector3();
-	const localVector2 = new three.Vector3();
-	const BANK_RIGHT = new three.Quaternion().setFromAxisAngle(new three.Vector3(0, 0, 1), Math.PI/2);
-	const BANK_LEFT = new three.Quaternion().setFromAxisAngle(new three.Vector3(0, 0, 1), -Math.PI/2);
-	const FORWARD$1 = new three.Vector3(0, 0, 1);
-	const ZERO_V = new three.Vector3(0, 0, 0);
-	const Y_180 = new three.Quaternion().setFromAxisAngle(new three.Vector3(0, 1, 0), Math.PI);
-	const Z_180 = new three.Quaternion().setFromAxisAngle(new three.Vector3(0, 0, 1), Math.PI);
-	const v1 = new three.Vector3();
-	const v2 = new three.Vector3();
-	const v3 = new three.Vector3();
-	const v4 = new three.Vector3();
-	const v5 = new three.Vector3();
-	const v6 = new three.Vector3();
-	const v7 = new three.Vector3();
-	const q1 = new three.Quaternion();
-	const q2 = new three.Quaternion();
-	const q3 = new three.Quaternion();
-	const q4 = new three.Quaternion();
-	const e1 = new three.Euler();
-	const m1 = new three.Matrix4();
+
+	class ActorParser {
+	  constructor(opt) {
+	    this.fileName = opt.fileName;
+	    this.bonePrefix = opt.bonePrefix;
+	    this.root = null;
+	    this.rootBone = null;
+	  }
+	  setRoot(root) {
+	    this.root = root;
+	    this.hips = this.root.getObjectByName(this.bonePrefix + 'Hips');
+	    this.shoulderR = this.root.getObjectByName(this.bonePrefix + 'RightShoulder');
+	    this.shoulderL = this.root.getObjectByName(this.bonePrefix + 'LeftShoulder');
+	    this.upperArmR = this.root.getObjectByName(this.bonePrefix + 'RightArm');
+	    this.upperArmL = this.root.getObjectByName(this.bonePrefix + 'LeftArm');
+	    this.lowerArmR = this.root.getObjectByName(this.bonePrefix + 'RightForeArm');
+	    this.lowerArmL = this.root.getObjectByName(this.bonePrefix + 'LeftForeArm');
+	    this.handR = this.root.getObjectByName(this.bonePrefix +'RightHand');
+	    this.handL = this.root.getObjectByName(this.bonePrefix + 'LeftHand');
+	    this.footR = this.root.getObjectByName(this.bonePrefix + 'RightFoot');
+	    this.footL = this.root.getObjectByName(this.bonePrefix + 'LeftFoot');
+	    this.root.updateWorldMatrix(true, true);
+	    setZForward(this);
+	    console.log(this.root);
+	    this.root.traverse( (object3D) => {
+	      if(object3D.isSkinnedMesh) {
+	      object3D.skeleton.calculateInverses();
+	      }
+	    });
+	    this.root.updateMatrixWorld(true);
+	  }
+	}
+
+	const BANK_RIGHT = new Quaternion().setFromAxisAngle(new Vector3(0, 0, 1), Math.PI/2);
+	const BANK_LEFT = new Quaternion().setFromAxisAngle(new Vector3(0, 0, 1), -Math.PI/2);
+	const Y_180 = new Quaternion().setFromAxisAngle(new Vector3(0, 1, 0), Math.PI);
+	const Z_180 = new Quaternion().setFromAxisAngle(new Vector3(0, 0, 1), Math.PI);
+	const v1 = new Vector3();
+	const v2 = new Vector3();
+	const v3 = new Vector3();
+	const v4 = new Vector3();
+	const v5 = new Vector3();
+	const v6 = new Vector3();
+	const v7 = new Vector3();
+	const q1 = new Quaternion();
+	const q2 = new Quaternion();
+	const q3 = new Quaternion();
+	const m1 = new Matrix4();
 	class ActorIK {
 	  constructor(parsedAvatar) {
 	    this.root = parsedAvatar.root;
@@ -30428,11 +30449,11 @@ void main() {
 	    this.handR = parsedAvatar.handR;
 	    this.handL = parsedAvatar.handL;
 	    this.setup();
-	    this.axesHelper1 = new three.AxesHelper(5);
+	    this.axesHelper1 = new AxesHelper(5);
 	    this.axesHelper1.material.depthTest = false;
 	    this.axesHelper1.material.depthWrite = false;
 	    this.axesHelper1.material.transparent = true;
-	    this.axesHelper2 = new three.AxesHelper(5);
+	    this.axesHelper2 = new AxesHelper(5);
 	    this.axesHelper2.material.depthTest = false;
 	    this.axesHelper2.material.depthWrite = false;
 	    this.axesHelper2.material.transparent = true;
@@ -30482,6 +30503,7 @@ void main() {
 	    lower.updateWorldMatrix(true);
 	  }
 	}
+
 	class AvatarIK extends ActorIK {
 	  constructor(parsedActor, trackedPerson) {
 	    super(parsedActor);
@@ -30489,14 +30511,15 @@ void main() {
 	    this.targetHandL = trackedPerson.handL;
 	  }
 	}
+
 	class TrackedPerson {
 	  constructor() {
-	    this.root = new three.Object3D();
-	    this.head = new three.Object3D();
+	    this.root = new Object3D();
+	    this.head = new Object3D();
 	    this.root.add(this.head);
-	    this.handR = new three.Object3D();
+	    this.handR = new Object3D();
 	    this.root.add(this.handR);
-	    this.handL = new three.Object3D();
+	    this.handL = new Object3D();
 	    this.root.add(this.handL);
 	  }
 	  tick() {
@@ -30513,158 +30536,6 @@ void main() {
 	    this.handL.quaternion.y *= -1;
 	    this.handL.quaternion.z *= -1;
 	  }
-	}
-	class BoneHelper extends three.Object3D {
-	  constructor(height, boneSize, axesSize) {
-	    super();
-	    if (height !== 0) {
-	      const geo = new three.ConeBufferGeometry(boneSize, height, 4);
-	      geo.applyMatrix(new three.Matrix4().makeRotationAxis(new three.Vector3(1, 0, 0), Math.PI/2));
-	      this.boneMesh = new three.Mesh(geo, new three.MeshBasicMaterial({
-	        color: 0xff0000,
-	        wireframe: true,
-	        depthTest: false,
-	        depthWrite: false,
-	        transparent: true,
-	      }));
-	    } else {
-	      this.boneMesh = new three.Object3D();
-	    }
-	    this.boneMesh.position.z = height / 2;
-	    this.add(this.boneMesh);
-	    this.axesHelper = new three.AxesHelper(axesSize);
-	    this.axesHelper.material.depthTest = false;
-	    this.axesHelper.material.depthWrite = false;
-	    this.axesHelper.material.transparent = true;
-	    this.add(this.axesHelper);
-	  }
-	}
-	class SkeletonAxisHelper extends three.Object3D {
-	  constructor(rootBone, { color, showBones, boneSize, showAxes, axesSize, wireframe } = {}) {
-	    super();
-	    boneSize = boneSize || 1;
-	    axesSize = axesSize || 5;
-	    if (!rootBone.isBone) {
-	      throw new Error('SkeletonAxisHelper must receive an bone instance.');
-	    }
-	    this.rootBone = rootBone;
-	    this._meshes = new Map();
-	    this.rootBone.traverse( (object3D) => {
-	      if(/hand\w+/i.test(object3D.name)) {
-	        return;
-	      }
-	      let length = 0;
-	      const child1 = object3D.children[0];
-	      if (child1) {
-	        length = child1.position.length();
-	      }
-	      const mesh = new BoneHelper(length, boneSize, axesSize);
-	      mesh.matrixAutoUpdate = false;
-	      this._meshes.set(object3D, mesh);
-	      this.add(mesh);
-	    });
-	    this.showBones = showBones !== undefined ? showBones : true;
-	    this.showAxes = showAxes !== undefined ? showAxes : true;
-	    this.wireframe = wireframe !== undefined ? wireframe : true;
-	    this.color = color || new three.Color(0xff0077);
-	  }
-	  get showBones() { return this._showBones; }
-	  set showBones(showBones) {
-	    if (showBones === this._showBones) {
-	      return;
-	    }
-	    for (let [joint, mesh] of this._meshes) {
-	      if (showBones) {
-	        mesh.add(mesh.boneMesh);
-	      } else {
-	        mesh.remove(mesh.boneMesh);
-	      }
-	    }
-	    this._showBones = showBones;
-	  }
-	  get showAxes() { return this._showAxes; }
-	  set showAxes(showAxes) {
-	    if (showAxes === this._showAxes) {
-	      return;
-	    }
-	    for (let [joint, mesh] of this._meshes) {
-	      if (showAxes) {
-	        mesh.add(mesh.axesHelper);
-	      } else {
-	        mesh.remove(mesh.axesHelper);
-	      }
-	    }
-	    this._showAxes = showAxes;
-	  }
-	  get wireframe() { return this._wireframe; }
-	  set wireframe(wireframe) {
-	    if (wireframe === this._wireframe) {
-	      return;
-	    }
-	    for (let [joint, mesh] of this._meshes) {
-	      if (mesh.boneMesh.material) {
-	        mesh.boneMesh.material.wireframe = wireframe;
-	      }
-	    }
-	    this._wireframe = wireframe;
-	  }
-	  get color() { return this._color; }
-	  set color(color) {
-	    if (this._color && this._color.equals(color)) {
-	      return;
-	    }
-	    color = (color && color.isColor) ? color : new three.Color(color);
-	    for (let [joint, mesh] of this._meshes) {
-	      if (mesh.boneMesh.material) {
-	        mesh.boneMesh.material.color = color;
-	      }
-	    }
-	    this._color = color;
-	  }
-	  updateMatrixWorld(force) {
-	    for (let [bone, mesh] of this._meshes) {
-	      mesh.matrix.copy(bone.matrixWorld);
-	    }
-	    super.updateMatrixWorld(force);
-	  }
-	}
-	class ActorParser {
-	  constructor(opt) {
-	    this.fileName = opt.fileName;
-	    this.bonePrefix = opt.bonePrefix;
-	    this.root = null;
-	    this.rootBone = null;
-	  }
-	  setRoot(root) {
-	    this.root = root;
-	    this.hips = this.root.getObjectByName(this.bonePrefix + 'Hips');
-	    this.shoulderR = this.root.getObjectByName(this.bonePrefix + 'RightShoulder');
-	    this.shoulderL = this.root.getObjectByName(this.bonePrefix + 'LeftShoulder');
-	    this.upperArmR = this.root.getObjectByName(this.bonePrefix + 'RightArm');
-	    this.upperArmL = this.root.getObjectByName(this.bonePrefix + 'LeftArm');
-	    this.lowerArmR = this.root.getObjectByName(this.bonePrefix + 'RightForeArm');
-	    this.lowerArmL = this.root.getObjectByName(this.bonePrefix + 'LeftForeArm');
-	    this.handR = this.root.getObjectByName(this.bonePrefix +'RightHand');
-	    this.handL = this.root.getObjectByName(this.bonePrefix + 'LeftHand');
-	    this.footR = this.root.getObjectByName(this.bonePrefix + 'RightFoot');
-	    this.footL = this.root.getObjectByName(this.bonePrefix + 'LeftFoot');
-	    this.root.updateWorldMatrix(true, true);
-	    setZForward(this);
-	    console.log(this.root);
-	    this.root.traverse( (object3D) => {
-	      if(object3D.isSkinnedMesh) {
-	      object3D.skeleton.calculateInverses();
-	      }
-	    });
-	    this.root.updateMatrixWorld(true);
-	  }
-	}
-	if (typeof window !== 'undefined' && typeof window.THREE === 'object') {
-	  window.THREE.Actor = {
-	    AvatarIK: AvatarIK,
-	    TrackedPerson: TrackedPerson,
-	    SkeletonAxisHelper: SkeletonAxisHelper
-	  };
 	}
 
 	var renderer, camera, scene, controls, trackedPerson, avatarIK;
@@ -30697,7 +30568,6 @@ void main() {
 	        roughnessMipmapper.dispose();
 	        actorParser.setRoot(gltf.scene.getObjectByName("Armature"));
 	        initAvatar(actorParser);
-	        scene.add(new SkeletonAxisHelper(actorParser.hips));
 	      });
 	  });
 	  camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 100);
